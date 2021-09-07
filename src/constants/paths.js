9dbaa2b05e33a0eec8c477882b7ac9b567712da8
Paras Solanki <parassolanki151@gmail.com>
2021-09-07 07:51:00 +0530

    modified url & add njkeyll file

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,4 +1,4 @@
 export const RECIPE_URL =
   "https://raw.githubusercontent.com/ParasSolanki/recipes-data/main/recipes.json";
 
-export const URL = "http://recipe-venture.com/recipes/";
+export const URL = "https://parassolanki.github.io/recipe-venture/#/recipes";
